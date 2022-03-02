585c2cca51d1b9a259744d9228fc0336f6c85524
Jeff <zhsu@amwares.com>
2022-03-02 16:34:53 +0800

    渐渐理解布局的写法。

--- src/main/C3S15Go.go
+++ src/main/C3S15Go.go
@@ -7,21 +7,26 @@ import (
 	. "github.com/lxn/walk/declarative"
 	"github.com/lxn/win"
 	"image/png"
-	"strings"
 	"time"
 )
 
 //go:embed images
 var imgs embed.FS
 
+type mainWndStuff struct {
+	mainWnd    *walk.MainWindow
+	chessBoard *walk.CustomWidget
+}
+
 func main() {
-	var inTE, outTE *walk.TextEdit
+	//bkFile, _ := imgs.Open("images/wood.jpg")
+	//bkImg, _ := jpeg.Decode(bkFile)
 
 	screenWidth := win.GetSystemMetrics(win.SM_CXSCREEN)
 	screenHeight := win.GetSystemMetrics(win.SM_CYSCREEN)
 	wndWidth := 800
 	wndHeight := 600
-	var mainWndP *walk.MainWindow
+	mws := new(mainWndStuff)
 	mainWnd := MainWindow{
 		Title:   "三炮十五兵 Go语言版",
 		MinSize: Size{Width: 600, Height: 400},
@@ -34,16 +39,39 @@ func main() {
 		Font:   Font{Family: "宋体", PointSize: 20},
 		Layout: VBox{},
 		Children: []Widget{
-			HSplitter{
+			Composite{
+				Layout: HBox{},
 				Children: []Widget{
-					TextEdit{AssignTo: &inTE},
-					TextEdit{AssignTo: &outTE, ReadOnly: true},
+					HSpacer{},
+					PushButton{
+						Text: "按钮，很多个字的按钮",
+					},
+					HSpacer{},
 				},
 			},
-			PushButton{
-				Text: "SCREAM",
-				OnClicked: func() {
-					_ = outTE.SetText(strings.ToUpper(inTE.Text()))
+			CustomWidget{
+				//Background: BitmapBrush{
+				//	Image: bkImg,
+				//},
+				//AssignTo: &mws.chessBoard,
+			},
+			//HSplitter{
+			//	Children: []Widget{
+			//		TextEdit{AssignTo: &inTE},
+			//		TextEdit{AssignTo: &outTE, ReadOnly: true},
+			//	},
+			//},
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					HSpacer{},
+					PushButton{
+						Text: "SCREAM",
+						//OnClicked: func() {
+						//	_ = outTE.SetText(strings.ToUpper(inTE.Text()))
+						//},
+					},
+					HSpacer{},
 				},
 			},
 		},
@@ -51,27 +79,27 @@ func main() {
 		},
 		OnSizeChanged: func() {
 			fmt.Printf("X=%d，Y=%d，宽度=%d，高度=%d\n",
-				mainWndP.X(), mainWndP.Y(),
-				mainWndP.Width(), mainWndP.Height())
+				mws.mainWnd.X(), mws.mainWnd.Y(),
+				mws.mainWnd.Width(), mws.mainWnd.Height())
 		},
 		OnBoundsChanged: func() {
 			fmt.Println("OnBoundsChanged: " +
-				fmt.Sprint(mainWndP.Bounds()))
+				fmt.Sprint(mws.mainWnd.Bounds()))
 		},
-		AssignTo: &mainWndP,
+		AssignTo: &mws.mainWnd,
 	}
 	go func() {
 		time.Sleep(3 * time.Second)
-		fmt.Println(mainWndP.Bounds())
+		fmt.Println(mws.mainWnd.Bounds())
 		mainWnd.Bounds.X = 300
-		fmt.Println(mainWndP.Bounds())
+		fmt.Println(mws.mainWnd.Bounds())
 	}()
 	pngFile, _ := imgs.Open("images/block.png")
 	img, _ := png.Decode(pngFile)
 	icon, _ := walk.NewIconFromImageForDPI(img, 300)
 	go func() {
 		time.Sleep(100 * time.Millisecond)
-		_ = mainWndP.SetIcon(icon)
+		_ = mws.mainWnd.SetIcon(icon)
 	}()
 	_, _ = mainWnd.Run()
 }
